72f093a0b79464e0295d96a49ba8319004fcd92c
zouazano <sou.asano11@gmail.com>
2019-01-12 09:23:31 +0900

    torima

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   has_many :user_items
   has_many :users, through: :user_items
+  has_many :tried_items
+  has_many :tried_users, through: :tried_items, source: :user
 end
