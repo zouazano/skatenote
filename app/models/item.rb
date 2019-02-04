517563dcbd1c91d1281e647744c5f725800c4146
zouazano <sou.asano11@gmail.com>
2019-02-04 21:30:21 +0900

    item's image added

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many :user_items, dependent: :destroy
   has_many :users, through: :user_items
-  has_many :want_items
+  has_many :want_items, dependent: :destroy
   has_many :want_users, through: :want_items, source: :user
 end
