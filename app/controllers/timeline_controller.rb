8ded8b68f85793ccb9c7c8bf0c748b2e59cb0f8a
zouazano <sou.asano11@gmail.com>
2019-04-21 08:11:21 +0900

    heroku deploy mae

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,5 +1,11 @@
 class TimelineController < ApplicationController
 	def index
-
+		@hogas = []
+    UserItem.all.each do |hoge|
+      @hogas << hoge
+    end
+    WantItem.all.each do |hoge|
+    	@hogas << hoge
+    end
 	end
 end
