37cc8fc1d520523ae7d58f7e0189d182d6c13fc1
zouazano <sou.asano11@gmail.com>
2019-04-28 10:57:33 +0900

    want back

--- app/controllers/want_items_controller.rb
+++ app/controllers/want_items_controller.rb
@@ -4,8 +4,7 @@ class WantItemsController < ApplicationController
     user=current_user
     @item=Item.find(params[:item_id])
     if WantItem.find_or_create_by(user_id: user.id, item_id:@item.id)
-      redirect_to item_path(@item)
-      flash = '"欲しい！"に追加しました！'
+      redirect_back(fallback_location: root_path)
     else
       redirect_to root_url
     end
