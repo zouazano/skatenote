fa8266b964b63ba498f050ffcf7ed6e8b98a26fb
zouazano <sou.asano11@gmail.com>
2019-05-15 13:07:15 +0900

    user_profile added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
  
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :profile])
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_29_221737) do
+ActiveRecord::Schema.define(version: 2019_05_15_035120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2019_04_29_221737) do
     t.datetime "remember_created_at"
     t.string "image_uid"
     t.string "image_name"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
